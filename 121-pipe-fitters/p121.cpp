c7630c069a1327be8032fff9941f502602825ced
ayushrajdahal <ayushrajdahal@gmail.com>
2025-02-18 22:24:08 -0600

    Consistent file naming across solutions.

--- 121-pipe-fitters/p121.cpp
+++ 121-pipe-fitters/p121.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,14 +15,16 @@ int skewA(const double rectangleHeight, const double rectangleWidth)
 {
     int rectangleWidthRoundedDown = static_cast<int>(rectangleWidth);
 
+    // finding no. of rows:
+    // - subtract height by 1 (sum of radius of the top/bottom pipes)
+    // - remaining height will be split into n-1 multiple of ROOT_THREE_BY_TWO (n = no. of rows), ignore decimals.
+    // - add 1 to get n.
     int numberOfRows = static_cast<int>(((rectangleHeight - 1.0) / ROOT_THREE_BY_TWO) + 1); // int type rounds down the result
 
     int numberOfEvenRows = numberOfRows / 2; // rounding down again
     int numberOfOddRows = numberOfRows - numberOfEvenRows;
 
-    int numberOfPipes = numberOfEvenRows * (rectangleWidthRoundedDown - 1) + numberOfOddRows * rectangleWidthRoundedDown; // there are n-1 elements in even rows, n elements in odd rows
-
-    return numberOfPipes;
+    return numberOfEvenRows * (rectangleWidthRoundedDown - 1) + numberOfOddRows * rectangleWidthRoundedDown; // there are n-1 elements in even rows, n elements in odd rows
 }
 
 int skewB(const double rectangleHeight, const double rectangleWidth)
@@ -32,9 +35,7 @@ int skewB(const double rectangleHeight, const double rectangleWidth)
 
     int numPipesInARow = (rectangleWidth - rectangleWidthRoundedDown) < 0.5 ? rectangleWidthRoundedDown - 1 : rectangleWidthRoundedDown;
 
-    int numberOfPipes = numPipesInARow * numberOfRows; // no. of pipes is the same throughout all rows here, unlike skewA.
-
-    return numberOfPipes;
+    return numPipesInARow * numberOfRows; // no. of pipes is the same throughout all rows here, unlike skewA.
 };
 
 int main()
@@ -45,13 +46,13 @@ int main()
     {
         if (a < 1 || b < 1)
         {
-            cout << 0 << endl;
+            cout << "0 grid" << endl;
             continue;
         }
 
         int gridOutput = grid(a, b); // this would equal grid(b,a) since we're doing the same operations to rows & cols. no comparison necessary
 
-        int maxSkewOutput = max(max(max(skewA(a, b), skewA(b, a)), skewB(a, b)), skewB(b, a)); // run all the combinations through all skew functions, get the max.
+        int maxSkewOutput = max({skewA(a, b), skewA(b, a), skewB(a, b), skewB(b, a)}); // run all the combinations through all skew functions, get the max.
 
         if (gridOutput >= maxSkewOutput) // we prefer grids to skews when the outputs are equal.
         {
